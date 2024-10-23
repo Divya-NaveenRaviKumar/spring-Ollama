cf361708021c202fad949ac2a5d7e3fd53ebf998
DivyaSomashekarN26 <divya.somashekar@n26.com>
2024-10-23 08:37:50 +0200

    Spring-ai : ollama hands-on

---
+++ src/main/java/org/pilot/springollama/ChatController.java
@@ -0,0 +1,28 @@
+package org.pilot.springollama;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/ai")
+    Map<String, String> completion(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of(
+                "completion",
+                chatClient.prompt()
+                        .user(message)
+                        .call()
+                        .content());
+    }
+}
